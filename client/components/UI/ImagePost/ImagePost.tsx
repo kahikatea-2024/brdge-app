ad4a1ec447c6f200dce84113d9116f238b2a2755
Jeevan Miranda <158778751+jeevan-miranda@users.noreply.github.com>
2024-06-20 10:41:42 +1200

    added db files and route files in backend to begin crud

--- client/components/UI/ImagePost/ImagePost.tsx
+++ client/components/UI/ImagePost/ImagePost.tsx
@@ -1,16 +1,4 @@
 import { HtmlHTMLAttributes, InputHTMLAttributes } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-type InputAttribues = InputHTMLAttributes<HTMLImageElement>
-
-export default function ImagePost({ className, ...rest }: InputAttribues) {
-  const ImageStyle = "object-scale-down h-48 w-96 ..."
-
-  return (
-    <img
-    className={twMerge(ImageStyle, className)}
-    {...rest}
-  />
-  )
-
-}
+type InputAttribues = InputHTMLAttributes<HTMLInputElement>
