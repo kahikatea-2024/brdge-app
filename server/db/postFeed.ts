17b7418e8e65d46ba5259a2e66fca82c86ea9e17
Jeevan Miranda <158778751+jeevan-miranda@users.noreply.github.com>
2024-06-20 11:08:28 +1200

    getallPosts added

--- server/db/postFeed.ts
+++ server/db/postFeed.ts
@@ -1,1 +1,15 @@
 import db from './connection.ts'
+
+//getAllPosts
+export async function getAllPosts() {
+  const post = await db('feed_posts').select()
+  return post
+}
+
+//addPost
+
+//editPost
+
+//deletPost
+
+//getPostById
--- server/routes/postFeed.ts
+++ server/routes/postFeed.ts
@@ -1,3 +1,25 @@
 import { Router } from 'express'
-
+import * as db from '../db/postFeed.ts'
 const router = Router()
+
+//getAllPosts route - checked in thunderclient, can receive results
+
+router.get('/', async (req, res) => {
+  try {
+    const posts = await db.getAllPosts()
+    res.json(posts)
+  } catch (error) {
+    console.error(`database error: ${error}`) //will return the error message in thunderclient
+    res.sendStatus(500)
+  }
+})
+
+//addPost route
+
+//editPOst route
+
+//deletePost route
+
+//getPOstbyId route
+
+export default router
