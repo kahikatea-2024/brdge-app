6e4374a1e5f51638bb8352ded455fe8a61ee6c2a
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-06-20 11:39:27 +1200

    Add Avatar component, update verifiedTick component

--- client/components/UI/ImagePost/ImagePost.tsx
+++ client/components/UI/ImagePost/ImagePost.tsx
@@ -1,15 +1,14 @@
-import { HtmlHTMLAttributes, InputHTMLAttributes } from 'react'
+import { ImgHTMLAttributes } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-interface ImageAttributes extends HtmlHTMLAttributes<HTMLImageElement>
+interface ImageAttributes extends ImgHTMLAttributes<HTMLImageElement> {}
 
-export default function ImagePost({ className, ...rest }: ImageAttributes) {
-  const ImageStyle = "object-scale-down h-48 w-96"
+export default function ImagePost({
+  className,
+  alt,
+  ...rest
+}: ImageAttributes) {
+  const ImageStyle = 'object-scale-down h-48 w-96'
 
-  return (
-    <img
-    className={twMerge(ImageStyle, className)}
-    {...rest}
-  />
-  )
+  return <img className={twMerge(ImageStyle, className)} alt={alt} {...rest} />
 }
--- client/components/UI/VerifiedTick/VerifiedTick.tsx
+++ client/components/UI/VerifiedTick/VerifiedTick.tsx
@@ -1,15 +1,20 @@
-import { HtmlHTMLAttributes, InputHTMLAttributes } from 'react'
+import { ImgHTMLAttributes } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-type InputAttribues = InputHTMLAttributes<HTMLImageElement>
+interface ImageAttributes extends ImgHTMLAttributes<HTMLImageElement> {}
 
-
-export default function VerifiedTick({className, ...rest}: InputAttribues) {
-
+export default function VerifiedTick({ className, ...rest }: ImageAttributes) {
   return (
     <div className="flex -space-x-4">
-        <img className={twMerge("w-12 h-12 rounded-full border-2 border-white", className)} {...rest}/>
+      <img
+        className={twMerge(
+          'h-12 w-12 rounded-full border-2 border-white',
+          className,
+        )}
+        src="images/tick-logo.png"
+        alt="verified"
+        {...rest}
+      />
     </div>
   )
-
 }
