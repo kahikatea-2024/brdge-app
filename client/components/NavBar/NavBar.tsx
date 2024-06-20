24bbf6de7beb5221d0f76685da5190fc26f2ac9f
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-06-21 10:32:54 +1200

    Add Auth0 integration and conditional authentication rendering

--- client/components/NavBar/NavBar.tsx
+++ client/components/NavBar/NavBar.tsx
@@ -1,9 +1,21 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import Avatar from '../UI/Avatar/Avatar'
 import Button from '../UI/Button/Button'
 import Logo from '../UI/Logo/Logo'
 import TextField from '../UI/Text field/TextField'
+import { IfAuthenticated, IfNotAuthenticated } from '../Authenticated'
 
 export default function NavBar() {
+  const { user, logout, loginWithRedirect } = useAuth0()
+
+  function handleSignOut() {
+    logout()
+  }
+
+  function handleSignIn() {
+    loginWithRedirect()
+  }
+
   return (
     <div className="border-gray-200 bg-lightGrey">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between gap-1 p-4">
@@ -15,11 +27,13 @@ export default function NavBar() {
           className="text-darkGray w-1/2 bg-extraLightGrey focus:ring-2 focus:ring-blue-500"
         />
         <div className="flex items-center gap-2">
-          <Avatar
-            src="https://i.pinimg.com/236x/5e/71/0b/5e710bb38b1cae44a3cae02342248eae.jpg"
-            size="small"
-          />
-          <Button>Sign out</Button>
+          <IfAuthenticated>
+            <Avatar src={user?.picture} size="small" />
+            <Button onClick={handleSignOut}>Sign out</Button>
+          </IfAuthenticated>
+          <IfNotAuthenticated>
+            <Button onClick={handleSignIn}>Sign in</Button>
+          </IfNotAuthenticated>
         </div>
       </div>
     </div>
