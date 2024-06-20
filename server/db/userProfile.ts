a51adbf0145cbbbad72e8719137d6478c10a6f61
Jeevan Miranda <158778751+jeevan-miranda@users.noreply.github.com>
2024-06-20 16:10:01 +1200

    user name search done

--- server/db/userProfile.ts
+++ server/db/userProfile.ts
@@ -1,1 +1,19 @@
 import db from './connection.ts'
+
+// getUserProfileByUsername
+
+export async function getUserProfileByUsername(userName: string) {
+  const res = db('profiles')
+    .join('users', 'profiles.user_id', 'users.user_id')
+    .where(
+      db.raw('lower(users.username) like ?', [`%${userName.toLowerCase()}%`]),
+    )
+
+  return res
+}
+
+//getUserProfileById
+export async function getUserProfileById(id: number) {
+  const res = db('profiles').where('user_id', id).first()
+}
+//editUserProfile
--- server/routes/userProfile.ts
+++ server/routes/userProfile.ts
@@ -1,3 +1,26 @@
 import { Router } from 'express'
-
+import * as db from '../db/userProfile.ts'
 const router = Router()
+
+//getUserProfileByUsername
+
+router.get('/search', async (req, res) => {
+  const query = req.query.q as string
+
+  if (!query) {
+    res.status(422).json({ message: 'Please provide a valid query' })
+  }
+  try {
+    const userProfile = await db.getUserProfileByUsername(query)
+    res.json(userProfile)
+  } catch (error) {
+    console.error(`database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
+
+export default router
+
+//getUserProfileById
+
+//
