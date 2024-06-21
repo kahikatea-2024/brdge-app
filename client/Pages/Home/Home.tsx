fdc89967a21a8321340c47998dc689be86436f3a
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-06-21 15:38:44 +1200

    Add Posts hook

--- client/Pages/Home/Home.tsx
+++ client/Pages/Home/Home.tsx
@@ -18,21 +18,22 @@ export default function Home() {
   }
 
   // Mock data
-  const mockPost = {
-    feed_post_id: 1,
-    user_id: 1,
-    content: `Hey, hey, hey! Donkey here, reporting live from Shreks swamp. Guess
-                what? Were knee-deep in CODING! Shreks debuggin like a pro, Fionas
-                cracking algorithms left and right, and Im still tryin to figure out
-                if &quot;honk&quot; counts as code. Join us for pixel-powered fun
-                and plenty of laughs in Far, Far Away! #DonkeysCodingChronicles
-                #SwampyTechAdventure`,
-    timestamp: '04 July',
-    image_url:
-      'https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/04/shrek-5-mike-myers.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5',
-  }
+  //   const mockPost = {
+  //     feed_post_id: 1,
+  //     user_id: 1,
+  //     content: `Hey, hey, hey! Donkey here, reporting live from Shreks swamp. Guess
+  //                 what? Were knee-deep in CODING! Shreks debuggin like a pro, Fionas
+  //                 cracking algorithms left and right, and Im still tryin to figure out
+  //                 if &quot;honk&quot; counts as code. Join us for pixel-powered fun
+  //                 and plenty of laughs in Far, Far Away! #DonkeysCodingChronicles
+  //                 #SwampyTechAdventure`,
+  //     timestamp: '04 July',
+  //     image_url:
+  //       'https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/04/shrek-5-mike-myers.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5',
+  //   }
 
-  const PostArray = [mockPost, mockPost, mockPost]
+  //   const PostArray = [mockPost, mockPost, mockPost]
+  console.log(user)
 
   return (
     <div className="gap-4 bg-darkGrey">
--- server/db/seeds/feed_posts.js
+++ server/db/seeds/feed_posts.js
@@ -9,7 +9,7 @@ export async function seed(knex) {
     {
       feed_post_id: 1,
       user_id: 1,
-      content: `Hey, hey, hey! Donkey here, reporting live from Shreks swamp. Guess at? Were knee-deep in CODING! Shreks debuggin like a pro, Fionas cracking algorithms left and right, and Im still tryin to figure out if &quot;honk&quot; counts as code. Join us for pixel-powered fun and plenty of laughs in Far, Far Away! #DonkeysCodingChronicles #SwampyTechAdventure`,
+      content: `Hey, hey, hey! Donkey here, reporting live from Shreks swamp. Guess what? Were knee-deep in CODING! Shreks debuggin like a pro, Fionas cracking algorithms left and right, and Im still tryin to figure out if &quot;honk&quot; counts as code. Join us for pixel-powered fun and plenty of laughs in Far, Far Away! #DonkeysCodingChronicles #SwampyTechAdventure`,
       timestamp: '2024-06-20 10:42:33',
       image_url:
         'https://static1.colliderimages.com/wordpress/wp-content/uploads/2023/04/shrek-5-mike-myers.jpg?q=50&fit=contain&w=1140&h=&dpr=1.5',
