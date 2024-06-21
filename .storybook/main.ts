74cc73e9d87b00f38f4caa1258caf0eca8f94292
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-06-21 12:05:32 +1200

    hotfix for react router in Storybook - Home and Profile components

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,6 +6,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    'storybook-addon-remix-react-router',
     {
       name: '@storybook/addon-styling',
       options: {
